1f385d55ac133a53c31c9fe753ba9d02866f18e1
Mohammad Rasim <mohammad.rasim96@gmail.com>
2023-03-27 16:37:49 +0300

    update to using localhost instead of a nextjs export

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,15 +1,28 @@
 // Native
-import { join } from 'path'
-import { format } from 'url'
+
+import { parse } from "url";
 
 // Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron'
-import isDev from 'electron-is-dev'
-import prepareNext from 'electron-next'
-
+import { BrowserWindow, app, ipcMain, IpcMainEvent } from "electron";
+import isDev from "electron-is-dev";
+import next from "next";
+import { createServer } from "http";
 // Prepare the renderer once the app is ready
-app.on('ready', async () => {
-  await prepareNext('./renderer')
+app.on("ready", async () => {
+  const nextApp = next({
+    dev: isDev,
+    dir: app.getAppPath() + "/renderer",
+  });
+  const requestHandler = nextApp.getRequestHandler();
+  // Build the renderer code and watch the files
+  await nextApp.prepare();
+  // Create a new native HTTP server (which supports hot code reloading)
+  createServer((req: any, res: any) => {
+    const parsedUrl = parse(req.url, true);
+    requestHandler(req, res, parsedUrl);
+  }).listen(3000, () => {
+    console.log("> Ready on http://localhost:3000");
+  });
 
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -17,26 +30,17 @@ app.on('ready', async () => {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: false,
-      preload: join(__dirname, 'preload.js'),
     },
-  })
+  });
 
-  const url = isDev
-    ? 'http://localhost:8000/'
-    : format({
-        pathname: join(__dirname, '../renderer/out/index.html'),
-        protocol: 'file:',
-        slashes: true,
-      })
-
-  mainWindow.loadURL(url)
-})
+  mainWindow.loadURL("http://localhost:3000/");
+});
 
 // Quit the app once all windows are closed
-app.on('window-all-closed', app.quit)
+app.on("window-all-closed", app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
-ipcMain.on('message', (event: IpcMainEvent, message: any) => {
-  console.log(message)
-  setTimeout(() => event.sender.send('message', 'hi from electron'), 500)
-})
+ipcMain.on("message", (event: IpcMainEvent, message: any) => {
+  console.log(message);
+  setTimeout(() => event.sender.send("message", "hi from electron"), 500);
+});
